03726f2e6db299005d929cd57645d5472bd9d4ac
协调员 <xietiaoyuan@xietiaoyuans-MacBook-Air.local>
2026-05-19 21:20:26 +0700

    add dic

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -1,3 +1,6 @@
+import sentences from "@/data/sentences.json";
+import words from "@/data/words.json";
+
 export const dynamic = "force-dynamic";
 
 export const fetchCache = "force-no-store";
@@ -11,7 +14,10 @@ if (!token)
 
 const bot = new Bot(token);
 bot.on("message:text", async (ctx) => {
-  await ctx.reply(ctx.message.text);
+  // await ctx.reply(ctx.message.text);
+  await ctx.api.sendMessage(ctx.chat.id, "<b>Hello, World!</b>", {
+    parse_mode: "HTML",
+  });
 });
 
 export const POST = webhookCallback(bot, "std/http");
