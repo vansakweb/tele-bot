78a005893e3edbb1394c73ad4076bbc94bc41eed
协调员 <xietiaoyuan@xietiaoyuans-MacBook-Air.local>
2026-05-20 21:07:51 +0700

    add dic

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -1,8 +1,7 @@
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
-
 import { dic } from "@/lib/dic";
-import { Bot, webhookCallback } from "grammy";
+import { Bot, webhookCallback, InputFile } from "grammy";
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
@@ -14,6 +13,13 @@ bot.on("message:text", async (ctx) => {
   await ctx.api.sendMessage(ctx.chat.id, dic(ctx.message.text), {
     parse_mode: "HTML",
   });
+
+  await bot.api.sendPhoto(ctx.chat.id, new InputFile("./image.png"), {
+    caption: "Hello",
+  });
 });
 
 export const POST = webhookCallback(bot, "std/http");
+ git add .
+ git commit -m "add dic"
+ git push -u origin main
